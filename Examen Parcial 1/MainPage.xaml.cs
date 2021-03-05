597a7b5b0079008891a2ea7111bd77ac4bbb87a4
Carlos-Fernando-Pineda <79342082+Carlos-Fernando-Pineda@users.noreply.github.com>
2021-03-05 17:22:07 -0600

    Add files via upload

---
+++ Examen Parcial 1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Examen_Parcial_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Numero.Text))
+            {
+                var numero = double.Parse(Numero.Text);
+
+                var original = (numero / 10) - 4;
+                Original.Text = original.ToString();
+            }
+            else
+            {
+                DisplayAlert("Datos erróneos",
+                    "Por favor, llena toda la información", 
+                    "Ok");
+            }
+            
+        }
+    }
+}
